033666a450d7d1303a82ffb7020b332bd2e3bd9e
erikgacitua <erik.gacitua.a@gmail.com>
2019-02-22 15:13:08 -0300

    nose

--- bff.php
+++ bff.php
@@ -4,6 +4,7 @@ ini_set('display_errors', '1');
 date_default_timezone_set('America/Santiago');
 
 require_once('./conexion.php');
+require_once('./microServicio.php');
 
 $accion = $_POST['accion'];
 
@@ -12,13 +13,13 @@ if ($accion == "createSchool") {
 	$nameInstitution = $_POST['institución'];
 	$commune = $_POST['comuna'];
 	$address = $_POST['direccion'];
-	$telefono = $_POST['telePhone'];
+	$telefono = $_POST['telefono'];
 	$director = $_POST['director'];
 	$emailDirector = $_POST['emailDirector'];
 	$contac = $_POST['contacto'];
 	$emailContac = $_POST['emailContac'];
 	$telePhoneContact = $_POST['telePhoneContact'];
 
-	print_r($_POST);
+	 prueba($nameInstitution,$commune,$address,$telefono,$director,$emailDirector,$contac,$emailContac,$telePhoneContact);
 }
 ?>
--- microServicio.php
+++ microServicio.php
@@ -5,4 +5,10 @@ date_default_timezone_set('America/Santiago');
 
 require_once('./conexion.php');
 
+function prueba($nameInstitution,$commune,$address,$telefono,$director,$emailDirector,$contac,$emailContac,$telePhoneContact){
+	
+	$sql = "call createSchool($nameInstitution,$commune,$address,$telefono,$director,$emailDirector,$contac,$emailContac,$telePhoneContact)";  
+	print($sql);
+}
+
 ?>
